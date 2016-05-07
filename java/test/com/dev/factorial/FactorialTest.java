63dfeb0720f5d3e8d6a5887a8e6c869aa33f95f1
David Huerta <dhuerta23@gmail.com>
2016-05-07 12:53:45 +0200

    Refactor: better name and follow conventions.

--- java/test/com/dev/factorial/FactorialTest.java
+++ java/test/com/dev/factorial/FactorialTest.java
@@ -1,22 +1,26 @@
-import com.dev.factorial.Factorial;
-import junit.framework.TestCase;
+package com.dev.factorial;
+
 import org.junit.Test;
 
-public class TestFactorial extends TestCase {
+import static org.junit.Assert.*;
 
+public class FactorialTest {
     @Test
     public void testFactorialZeroShouldBeOne() {
         assertEquals(1, Factorial.calculate(0));
     }
 
+    @Test
     public void testFactorialOneShouldBeOne() {
         assertEquals(1, Factorial.calculate(1));
     }
 
+    @Test
     public void testFactorialThreeShouldBeSix() {
         assertEquals(6, Factorial.calculate(3));
     }
 
+    @Test
     public void testFactorialForBiggerNumbers() {
         assertEquals(1307674368000L, Factorial.calculate(15));
     }
