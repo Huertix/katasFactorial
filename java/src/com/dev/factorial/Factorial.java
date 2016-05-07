1294453d65e207a7190b974a1ca27d8741388789
David Huerta <dhuerta23@gmail.com>
2016-05-07 12:39:26 +0200

    Refactor

--- java/src/com/dev/factorial/Factorial.java
+++ java/src/com/dev/factorial/Factorial.java
@@ -3,7 +3,6 @@ package com.dev.factorial;
 
 public class Factorial {
     public static long calculate(long n) {
-        if (n<=1) return 1;
-        return calculate(n-1) * n;
+        return n>1 ? calculate(n-1) * n : 1;
     }
 }
