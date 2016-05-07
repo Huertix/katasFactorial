7b4ebebc4fce3f7c01caf7e94663108bf59d61b3
David Huerta <dhuerta23@gmail.com>
2016-05-07 11:39:09 +0200

    Recursive factorial.

--- js/test.js
+++ js/test.js
@@ -2,18 +2,27 @@
  Factorial: https://en.wikipedia.org/wiki/Factorial
  Notation: n! = n * (n-1) * .. * 1
  Example: 5! = 5*4*3*2*1 = 120
-TODO
-	Factorial(0) = 1;
-	Factorial(1) = 1;
-	Factorial(3) = 6;
-	Factorial(15) = 1.307.674.368.000;
 */
 
 function factorial(n) {
-	return 1;
+	if (n<=1) return 1;
+	return factorial(n-1) * n;
 }
 describe("Factorial test", function(){
 	it("0! should be 1", function(){
 		expect(factorial(0)).toBe(1);
 	});
+
+	it("1! should be 1", function(){
+		expect(factorial(1)).toBe(1);
+	});
+
+	it("3! should be 6", function(){
+		expect(factorial(3)).toBe(6);
+	});
+
+	it("15! should be 1.307.674.368.000", function(){
+		expect(factorial(15)).toBe(1307674368000);
+	});
+    
 });
