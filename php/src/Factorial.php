49413981b843597276c1f8fc41b4b6aa0383ba7c
David Huerta <dhuerta23@gmail.com>
2016-05-07 17:19:48 +0200

    Fix infinite loop issue.

--- php/src/Factorial.php
+++ php/src/Factorial.php
@@ -5,6 +5,6 @@ class Factorial
 {
     public function calculate($n)
     {
-        return $n > 1 ? $n*$this->calculate($n) : 1;
+        return $n > 1 ? $n*$this->calculate($n-1) : 1;
     }
 }
--- php/tests/FactorialTest.php
+++ php/tests/FactorialTest.php
@@ -16,5 +16,20 @@ class FactorialTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals(1, $this->factorial->calculate(0));
     }
-}
 
+    /**
+     * @test
+     */
+    public function shouldReturnOneWhenOne()
+    {
+        $this->assertEquals(1, $this->factorial->calculate(1));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnSixWhenThree()
+    {
+        $this->assertEquals(6, $this->factorial->calculate(3));
+    }
+}
