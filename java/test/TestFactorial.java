0679066c85132efd377f27f8376c706fcf50cdd6
David Huerta <dhuerta23@gmail.com>
2016-05-07 12:19:30 +0200

    Initial import java kata.

---
+++ java/test/TestFactorial.java
@@ -0,0 +1,11 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TestFactorial extends TestCase {
+
+    @Test
+    public void testShouldBeTrue() {
+        assertTrue(true);
+    }
+}
+
